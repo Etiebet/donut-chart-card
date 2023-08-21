7a508ae0938b15dd9a0d548ea486bfe97818418f
Etiebet Nkanga <etiebetnkanga@gmail.com>
2023-08-21 09:13:01 +0200

    Your commit message here

---
+++ src/components/DonutChartCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="donut-chart-card">
+    <div class="chart-container">
+      <apexchart type="donut" :options="chartOptions" :series="chartSeries" />
+    </div>
+    <!-- Other card elements go here -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import VueApexCharts from 'vue3-apexcharts';
+
+export default {
+  name: 'DonutChartCard',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      chartOptions: {
+        // Set up your chart options here
+      },
+      chartSeries: [], // Populate this array with the data
+    };
+  },
+  async created() {
+    await this.fetchChartData();
+  },
+  methods: {
+    async fetchChartData() {
+      try {
+        const response = await axios.get('/path/to/your/data.json');
+        this.chartSeries = response.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Your card styles go here */
+</style>
